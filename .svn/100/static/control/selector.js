605a69ab6ff4040b42109e19056c4b95831a113e
daqiu.lym <daqiu.lym@taobao.com>
2014-04-03 02:08:28 +0800

    add page

--- .svn/100/static/control/selector.js
+++ .svn/100/static/control/selector.js
@@ -6,7 +6,6 @@ define(function(require,exports,module){
             //console.log()
             var uiEl = $(this),
                 uiTextEl = uiEl.find(".ui-btn-text"),
-                defaultVal = uiTextEl.attr('data-tip'),
                 selectEl = uiEl.find("select"),
                 selectedIndex = selectEl.get(0).selectedIndex;
             
@@ -16,6 +15,35 @@ define(function(require,exports,module){
                 var text = $(this).get(0).options[index].text;
                 uiTextEl.html(text);
             });
+        });
+        return this;
+    }
+
+    $.fn.uiradio=function(){
+        this.each(function(){
+            //console.log()
+            var radioWrapEl = $(this), //每个radioWrapEl作为一组
+                radioEls = radioWrapEl.find(".ui-radio-inp");
+            var checkthis = function(el){
+                $(el).parent(".ui-radio-inner").addClass("ui-radio-checked")
+            }
+            var uncheckthis = function(){
+                radioWrapEl.find('.ui-radio-checked').removeClass("ui-radio-checked");
+            }
+
+            radioEls.each(function(){
+                if($(this).prop("checked")){
+                    checkthis(this);
+                }
+            });
+            
+            //radioEls.each(function(){
+                
+            radioEls.change(function(){
+                uncheckthis();
+                checkthis(this);
+            });
+            //});
         });
         return this;
     }
